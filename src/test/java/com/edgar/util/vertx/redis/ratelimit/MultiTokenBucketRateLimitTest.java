462d3e73d1e8c57698f119c2aa25ba902ce67589
edgar615 <edgar615@gmail.com>
2017-06-22 15:31:54 +0800

    multi

--- src/test/java/com/edgar/util/vertx/redis/ratelimit/MultiTokenBucketRateLimitTest.java
+++ src/test/java/com/edgar/util/vertx/redis/ratelimit/MultiTokenBucketRateLimitTest.java
@@ -24,7 +24,7 @@ import java.util.concurrent.atomic.AtomicInteger;
  * Created by edgar on 17-5-28.
  */
 @RunWith(VertxUnitRunner.class)
-public class TokenBucketTest {
+public class MultiTokenBucketRateLimitTest {
 
   private RedisClient redisClient;
 
@@ -34,10 +34,10 @@ public class TokenBucketTest {
   public void setUp() {
     vertx = Vertx.vertx();
     redisClient = RedisClient.create(vertx, new RedisOptions()
-            .setHost("10.11.0.31"));
+    .setHost("10.11.0.31"));
     AtomicBoolean complete = new AtomicBoolean();
     RedisDeletePattern.create(redisClient)
-        .deleteByPattern("token_bucket*", ar -> {complete.set(true);});
+        .deleteByPattern("token.bucket*", ar -> {complete.set(true);});
     Awaitility.await().until(() -> complete.get());
   }
 
@@ -49,18 +49,21 @@ public class TokenBucketTest {
       if (ar.succeeded()) {
         complete.set(true);
       } else {
+        ar.cause().printStackTrace();
         complete.set(false);
       }
     });
-    TokenBucket tokenBucket = new TokenBucket(vertx, redisClient, future);
+    MultiTokenBucket tokenBucket = new MultiTokenBucket(vertx, redisClient, future);
     Awaitility.await().until(() -> complete.get());
     AtomicInteger req = new AtomicInteger();
     List<LimitResult> result = new ArrayList<>();
     String subject = UUID.randomUUID().toString();
-    TokenBucketRule options =
-        new TokenBucketRule(subject).setBurst(3)
-                .setRefillTime(2000);
-    tokenBucket.tokenBucket(3, options, ar -> {
+    TokenBucketRule rule =
+            new TokenBucketRule(subject).setBurst(3)
+                    .setRefillTime(2000);
+    List<TokenBucketRule> rules = new ArrayList<>();
+    rules.add(rule);
+    tokenBucket.tokenBucket(3, rules, ar -> {
       if (ar.failed()) {
         ar.cause().printStackTrace();
         testContext.fail();
@@ -74,7 +77,7 @@ public class TokenBucketTest {
     } catch (InterruptedException e) {
       e.printStackTrace();
     }
-    tokenBucket.tokenBucket(1, options, ar -> {
+    tokenBucket.tokenBucket(1, rules, ar -> {
       if (ar.failed()) {
         testContext.fail();
       } else {
@@ -87,7 +90,7 @@ public class TokenBucketTest {
     } catch (InterruptedException e) {
       e.printStackTrace();
     }
-    tokenBucket.tokenBucket(1, options, ar -> {
+    tokenBucket.tokenBucket(1, rules, ar -> {
       if (ar.failed()) {
         testContext.fail();
       } else {
@@ -101,7 +104,7 @@ public class TokenBucketTest {
     } catch (InterruptedException e) {
       e.printStackTrace();
     }
-    tokenBucket.tokenBucket(1, options, ar -> {
+    tokenBucket.tokenBucket(1, rules, ar -> {
       if (ar.failed()) {
         testContext.fail();
       } else {
@@ -126,27 +129,36 @@ public class TokenBucketTest {
     Assert.assertTrue(result.get(3).passed());
   }
 
+
   @Test
-  public void testBucket3Refill3In2000(TestContext testContext) {
+  public void testBucket3Refill3In1000AndBucket5Refill1In2000(TestContext testContext) {
     AtomicBoolean complete = new AtomicBoolean();
     Future<Void> future = Future.future();
     future.setHandler(ar -> {
       if (ar.succeeded()) {
         complete.set(true);
       } else {
+        ar.cause().printStackTrace();
         complete.set(false);
       }
     });
-    TokenBucket tokenBucket = new TokenBucket(vertx, redisClient, future);
+    MultiTokenBucket tokenBucket = new MultiTokenBucket(vertx, redisClient, future);
     Awaitility.await().until(() -> complete.get());
     AtomicInteger req = new AtomicInteger();
     List<LimitResult> result = new ArrayList<>();
     String subject = UUID.randomUUID().toString();
-    TokenBucketRule options =
+    TokenBucketRule rule1 =
             new TokenBucketRule(subject).setBurst(3)
                     .setRefillAmount(3)
+                    .setRefillTime(1000);
+    TokenBucketRule rule2 =
+            new TokenBucketRule(UUID.randomUUID().toString()).setBurst(5)
                     .setRefillTime(2000);
-    tokenBucket.tokenBucket(3, options, ar -> {
+    List<TokenBucketRule> rules = new ArrayList<>();
+    rules.add(rule1);
+    rules.add(rule2);
+    //成功
+    tokenBucket.tokenBucket(3, rules, ar -> {
       if (ar.failed()) {
         ar.cause().printStackTrace();
         testContext.fail();
@@ -160,76 +172,8 @@ public class TokenBucketTest {
     } catch (InterruptedException e) {
       e.printStackTrace();
     }
-    tokenBucket.tokenBucket(1, options, ar -> {
-      if (ar.failed()) {
-        testContext.fail();
-      } else {
-        req.incrementAndGet();
-        result.add(ar.result());
-      }
-    });
-    try {
-      TimeUnit.SECONDS.sleep(1);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    }
-    tokenBucket.tokenBucket(1, options, ar -> {
-      if (ar.failed()) {
-        testContext.fail();
-      } else {
-        req.incrementAndGet();
-        result.add(ar.result());
-      }
-    });
-    Awaitility.await().until(() -> req.get() == 3);
-    System.out.println(result);
-
-    Assert.assertEquals(2, result.stream().filter(resp -> resp.passed()).count());
-    Assert.assertEquals(0, result.get(0).details().get(0).remaining());
-    Assert.assertTrue(result.get(0).passed());
-
-    Assert.assertEquals(0, result.get(1).details().get(0).remaining());
-    Assert.assertFalse(result.get(1).passed());
-
-    Assert.assertEquals(2, result.get(2).details().get(0).remaining());
-    Assert.assertTrue(result.get(2).passed());
-
-  }
-
-  @Test
-  public void testBurst(TestContext testContext) {
-    AtomicBoolean complete = new AtomicBoolean();
-    Future<Void> future = Future.future();
-    future.setHandler(ar -> {
-      if (ar.succeeded()) {
-        complete.set(true);
-      } else {
-        complete.set(false);
-      }
-    });
-    TokenBucket tokenBucket = new TokenBucket(vertx, redisClient, future);
-    Awaitility.await().until(() -> complete.get());
-    AtomicInteger req = new AtomicInteger();
-    List<LimitResult> result = new ArrayList<>();
-    String subject = UUID.randomUUID().toString();
-    TokenBucketRule options =
-            new TokenBucketRule(subject).setBurst(3)
-                    .setRefillTime(2000);
-    tokenBucket.tokenBucket(1, options, ar -> {
-      if (ar.failed()) {
-        ar.cause().printStackTrace();
-        testContext.fail();
-      } else {
-        req.incrementAndGet();
-        result.add(ar.result());
-      }
-    });
-    try {
-      TimeUnit.SECONDS.sleep(1);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    }
-    tokenBucket.tokenBucket(2, options, ar -> {
+    //规则1填充3个，通过
+    tokenBucket.tokenBucket(1, rules, ar -> {
       if (ar.failed()) {
         testContext.fail();
       } else {
@@ -242,7 +186,8 @@ public class TokenBucketTest {
     } catch (InterruptedException e) {
       e.printStackTrace();
     }
-    tokenBucket.tokenBucket(1, options, ar -> {
+    //规则2不通过
+    tokenBucket.tokenBucket(2, rules, ar -> {
       if (ar.failed()) {
         testContext.fail();
       } else {
@@ -256,7 +201,8 @@ public class TokenBucketTest {
     } catch (InterruptedException e) {
       e.printStackTrace();
     }
-    tokenBucket.tokenBucket(1, options, ar -> {
+    //规则2秒填充1个，连续两个请求只能通过一个
+    tokenBucket.tokenBucket(1, rules, ar -> {
       if (ar.failed()) {
         testContext.fail();
       } else {
@@ -264,20 +210,44 @@ public class TokenBucketTest {
         result.add(ar.result());
       }
     });
-    Awaitility.await().until(() -> req.get() == 4);
+    try {
+      TimeUnit.MILLISECONDS.sleep(200);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    tokenBucket.tokenBucket(1, rules, ar -> {
+      if (ar.failed()) {
+        testContext.fail();
+      } else {
+        req.incrementAndGet();
+        result.add(ar.result());
+      }
+    });
+    Awaitility.await().until(() -> req.get() == 5);
     System.out.println(result);
 
-    Assert.assertEquals(2, result.stream().filter(resp -> resp.passed()).count());
-    Assert.assertEquals(2, result.get(0).details().get(0).remaining());
+    Assert.assertEquals(3, result.stream().filter(resp -> resp.passed()).count());
+
     Assert.assertTrue(result.get(0).passed());
+    Assert.assertEquals(0, result.get(0).details().get(0).remaining());
+    Assert.assertEquals(2, result.get(0).details().get(1).remaining());
 
-    Assert.assertEquals(0, result.get(1).details().get(0).remaining());
     Assert.assertTrue(result.get(1).passed());
+    Assert.assertEquals(2, result.get(1).details().get(0).remaining());
+    Assert.assertEquals(1, result.get(1).details().get(1).remaining());
 
-    Assert.assertEquals(0, result.get(2).details().get(0).remaining());
     Assert.assertFalse(result.get(2).passed());
+    Assert.assertEquals(3, result.get(2).details().get(0).remaining());
+    Assert.assertEquals(1, result.get(2).details().get(1).remaining());
 
-    Assert.assertEquals(0, result.get(3).details().get(0).remaining());
-    Assert.assertFalse(result.get(3).passed());
+    Assert.assertTrue(result.get(3).passed());
+    Assert.assertEquals(2, result.get(3).details().get(0).remaining());
+    Assert.assertEquals(0, result.get(3).details().get(1).remaining());
+
+    Assert.assertFalse(result.get(4).passed());
+    Assert.assertEquals(2, result.get(4).details().get(0).remaining());
+    Assert.assertEquals(0, result.get(4).details().get(1).remaining());
   }
+
+
 }
